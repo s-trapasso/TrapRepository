95fbdd71a881a2682e8345109293b3ad04c503d8
s-trapasso <147837910+s-trapasso@users.noreply.github.com>
2025-10-30 12:28:05 +0100

    Delete RESTful Web API directory

--- RESTful Web API/Models/Villa.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RESTful_Web_API.Models
-{
-    public class Villa
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime CreatedDate { get; set; }
-    }
-}
